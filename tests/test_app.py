429a96d1964ac9260088167ddd532048a2444c92
Dawid Karpczak <karpczak@gmail.com>
2025-04-08 16:07:51 +0200

    Fix tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import pytest
+from flask import url_for  # Dodaj ten import
 
 # Dodaj ścieżkę do katalogu, w którym znajduje się app.py
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -23,10 +24,10 @@ def test_homepage(client):
 
 
 def test_contact_form(client):
-    """Testujemy formularz kontaktowy.""" 
+    """Testujemy formularz kontaktowy."""
     response = client.post('/contact', data={'name': 'John', 'message': 'Hello'})
     assert response.status_code == 302  # Status 302 oznacza przekierowanie
-    assert response.location == 'http://localhost:5000/contact'  # Oczekujemy przekierowania na stronę kontaktu
+    assert response.location == url_for('contact', _external=True)  # Oczekujemy pełny URL
 
 def test_not_found(client):
     """Testujemy stronę 404 dla nieistniejącego endpointa.""" 
@@ -34,25 +35,25 @@ def test_not_found(client):
     assert response.status_code == 404
 
 def test_about_page(client):
-    """Testujemy stronę o aplikacji."""
+    """Testujemy stronę o aplikacji.""" 
     response = client.get('/about')
     assert response.status_code == 200
     assert b'DevOps demo app' in response.data
 
 def test_login_success(client):
-    """Testujemy poprawne logowanie."""
+    """Testujemy poprawne logowanie.""" 
     response = client.post('/login', data={'username': 'admin', 'password': 'password'})
     assert response.status_code == 302  # Status 302 oznacza przekierowanie po udanym logowaniu
     assert response.location == '/'  # Oczekujemy tylko ścieżki, nie pełnego URL
 
 def test_login_failure(client):
-    """Testujemy niepoprawne logowanie."""
+    """Testujemy niepoprawne logowanie.""" 
     response = client.post('/login', data={'username': 'wronguser', 'password': 'wrongpass'})
     assert response.status_code == 200  # Powinno pozostać na stronie logowania
     assert b'Invalid credentials. Please try again.' in response.data  # Oczekiwany komunikat błędu
 
 def test_change_password_success(client):
-    """Testujemy udaną zmianę hasła."""
+    """Testujemy udaną zmianę hasła.""" 
     response = client.post('/change-password', data={
         'new_password': 'newpassword123',
         'confirm_password': 'newpassword123'
@@ -64,4 +65,3 @@ def test_change_password_success(client):
     # Teraz sprawdzimy, czy komunikat o sukcesie pojawi się na stronie głównej
     response = client.get('/')
     assert b'Password changed successfully!' in response.data  # Oczekiwany komunikat sukcesu
-
